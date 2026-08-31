660e5299311e9334a72fd07d175628ec6ae0e862
 Aleksei Zelentsov <37956463+zelentsov-dev@users.noreply.github.com>
2026-08-31 16:21:55 +0000

    apple-ads-mcp 0.3.6

--- Formula/apple-ads-mcp.rb
+++ Formula/apple-ads-mcp.rb
@@ -5,21 +5,21 @@ class AppleAdsMcp < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.5/apple-ads-mcp_darwin_arm64_0.3.5.tar.gz"
-      sha256 "403cfe7539227c2c31d3f7e28597bbddd436ec7e3feb1f2f1af2ea8be39d0975"
+      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.6/apple-ads-mcp_darwin_arm64_0.3.6.tar.gz"
+      sha256 "b60a682f876288936c8fac5e184afab505e13ee1fdba114f42eabdf385128c1c"
     else
-      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.5/apple-ads-mcp_darwin_amd64_0.3.5.tar.gz"
-      sha256 "001443885e0b296691288e8375716d0b755d03c6c997b2b7ddb84f8605e433ef"
+      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.6/apple-ads-mcp_darwin_amd64_0.3.6.tar.gz"
+      sha256 "d48fadaff90acd0cff7552893b101870d5515d2a9dff056c37114040d0d38837"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.5/apple-ads-mcp_linux_arm64_0.3.5.tar.gz"
-      sha256 "5a12deaf36155d784cbb76f5c0acacab89798df8295147838dc4bd917ffeb5df"
+      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.6/apple-ads-mcp_linux_arm64_0.3.6.tar.gz"
+      sha256 "cd460973bfdb75d16db2cfc4c8ec101acf2e2738e130000a05ac1cc281f10c87"
     else
-      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.5/apple-ads-mcp_linux_amd64_0.3.5.tar.gz"
-      sha256 "d2835dff37366ae1d92075801c71bd8d7ba2fca2ac785e420273edab8009ef4a"
+      url "https://github.com/zelentsov-dev/apple-ads-mcp/releases/download/v0.3.6/apple-ads-mcp_linux_amd64_0.3.6.tar.gz"
+      sha256 "c7e541b04ed138e8042713967ba4a8d1fcc874fb5d2a5324fdb5a792424ccd95"
     end
   end
 
